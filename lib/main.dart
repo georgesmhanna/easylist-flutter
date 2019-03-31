c25c75b433a3530e3ca9298262119ba0da8c237b
georgesmhanna <georgesmhanna92@gmail.com>
2019-03-31 15:30:43 +0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(title: Text('EasyList')),
+            body: Column(children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  child: Text('Add Product'),
+                  onPressed: () {
+                    setState(() {
+                      _products.add('Advanced Food Tester');
+                    });
+                  },
+                ),
+              ),
+            ])));
+  }
+}
